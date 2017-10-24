8f9591fac6f793842890cca88271796697b1ddc4
MARTIN TORRES <mat885@hotmail.com>
2017-10-24 17:08:48 -0300

    Detección de fines de semana

--- Guia1/Guia1/ViewModels/HojaCalculoViewModel.cs
+++ Guia1/Guia1/ViewModels/HojaCalculoViewModel.cs
@@ -1,15 +1,20 @@
 ﻿using GalaSoft.MvvmLight.Command;
+using Guia1.Interfaces;
 using Guia1.Models;
 using Guia1.Services;
 using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Navigation;
+using Syncfusion.XlsIO;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace Guia1.ViewModels
 {
@@ -394,9 +399,19 @@ namespace Guia1.ViewModels
 
         }
         //Despliegue de semanas o días para calcular
-        private void Display()
+        private async void Display()
         {
+            if(DateSelected.DayOfWeek==DayOfWeek.Saturday || DateSelected.DayOfWeek== DayOfWeek.Sunday)
+            {
+                await dialogService.ShowMessage("Mensaje", "La fecha seleccionada " + DateSelected.ToString().Remove(10) + " pertenece al fin de semana. Por lo que se tomará en cuenta el día de semana más próximo.");
+            }
+
             var difDias = 0;
+            if(int.Parse(CantSemanas)<= TiempoFabricacionTotal())
+            {
+                await dialogService.ShowMessage("Error", "La cantidad de días debe ser mayor, ya que el tiempo de fabricación total será de "+ TiempoFabricacionTotal()+" días");
+                return;
+            }
             foreach (var productoA in ProductoPrincipal.Productos)
             {
 
@@ -410,6 +425,7 @@ namespace Guia1.ViewModels
 
                 difDias = DateSelected.DayOfYear - DateTime.Today.DayOfYear;
 
+                var contador = 0;
                 //Agregado de objetos a la lista, empieza en 1 para poder visualizar desde ese número y no desde 0
                 for (int i = 1; i <= (int.Parse(CantSemanas) + difDias); i++)
                 {
@@ -417,14 +433,34 @@ namespace Guia1.ViewModels
                     Semanas = new SemanasA();
                     if (DateSelected.Date == DateTime.Today)
                     {
-                        //Agregado de fecha al lado del dia
-                        Semanas.FechaDias = DateSelected.Date.AddDays(i - 1).ToString().Remove(10);
+                        //Agregado de fecha 
+                        if (DateSelected.Date.AddDays(contador).DayOfWeek== DayOfWeek.Saturday || DateSelected.Date.AddDays(contador).DayOfWeek == DayOfWeek.Sunday)
+                        {
+              
+                            contador = contador + 2;
+                            Semanas.FechaDias = DateSelected.Date.AddDays(contador).ToString().Remove(10);
+                        }
+                        else {
+                        Semanas.FechaDias = DateSelected.Date.AddDays(contador).ToString().Remove(10);
+                        }
+
+
                     }
                     else
                     {
                         //Agregado de fecha al lado del dia con la resta necesaria para empezar la cuenta desde el día de hoy
-                        Semanas.FechaDias = DateSelected.Date.AddDays(i - difDias - 1).ToString().Remove(10);
+                        if (DateSelected.Date.AddDays(contador - difDias).DayOfWeek == DayOfWeek.Saturday || DateSelected.Date.AddDays(contador - difDias).DayOfWeek == DayOfWeek.Sunday)
+                        {
+
+                            contador = contador + 2;
+                            Semanas.FechaDias = DateSelected.Date.AddDays(contador - difDias).ToString().Remove(10);
+                        }
+                        else
+                        {
+                            Semanas.FechaDias = DateSelected.Date.AddDays(contador - difDias).ToString().Remove(10);
+                        }
                     }
+                    contador++;
                     Semanas.IsEnabled = true;
                     Semanas.SemanasNumero = i.ToString();
                     Semanas.SemanasDiasCalculo = SemanasDiasCalculo.Replace("s", "");
@@ -434,7 +470,8 @@ namespace Guia1.ViewModels
                 //Bloqueo de días anteriores a los permitidos por la fecha de fabricación
                 if (DateSelected.Date == DateTime.Today)
                 {
-                    for (int i = 0; i < int.Parse(productoA.TiempoFabricacion); i++)
+                    
+                        for (int i = 0; i < TiempoFabricacionTotal(); i++)
                     {
                         productoA.Semanas[i].IsEnabled = false;
                     }
@@ -450,6 +487,21 @@ namespace Guia1.ViewModels
                 IsEnabled = true;
             }
             SemanasCollection = new ObservableCollection<SemanasA>(ProductoPrincipal.Productos[0].Semanas);
+        }
+
+        private  int TiempoFabricacionTotal()
+        {
+            var TiempoTotal = 0;
+            //Separa en grupos definidos por la dependencia
+            foreach (var grupo in ProductoPrincipal.Productos.GroupBy(x=>x.Dependencia).ToList())
+            {
+
+               //Saca El máximo valor de cada Key del grupo creado
+                 TiempoTotal = TiempoTotal + int.Parse(grupo.Max(x => x.TiempoFabricacion));
+                
+            }
+              
+            return TiempoTotal;
         }
 
         public ICommand CalcularCommand
@@ -596,7 +648,89 @@ namespace Guia1.ViewModels
                 return new RelayCommand(ReCalcular);
             }
         }
+        public ICommand XlsCommand
+        {
+            get
+            {
+                return new RelayCommand(Xls);
+            }
+        }
 
+        private async void Xls()
+        {
+            if (ProductoPrincipal.Calculo == false)
+            {
+                await dialogService.ShowMessage("Error", "Se debe realizar, en primera medida, el cálculo");
+                return;
+            }
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+
+                cont = 0;
+                //Seleccionar versión de Excel 2013
+                excelEngine.Excel.DefaultVersion = ExcelVersion.Excel2013;
+
+                //Crear workbook con una hoja de trabajo
+                IWorkbook workbook = excelEngine.Excel.Workbooks.Create(1);
+
+
+                foreach (var productoA in ProductoPrincipal.Productos)         
+                {
+                    CantSemanas = productoA.Semanas.Count.ToString();
+                   
+                   
+                    //Acceder a la primera hoja de trabajo desde la instancia de workbook
+                    IWorksheet worksheet = workbook.Worksheets[0];
+
+                    IMigrantRange migrantRange = worksheet.MigrantRange;
+                    // Writing Data.
+                    //cont aumenta en 7 la posición de las filas en cada producto, las columnas dependen de los días elegidos
+                       
+
+                        worksheet["A" + (cont+1).ToString()].Text = productoA.Nombres;
+                  
+                        migrantRange["A" + (2 + cont).ToString()].Text = "Requerimiento Bruto";
+               
+                        migrantRange["A" + (3 + cont).ToString()].Text = "Inventario Inicial";
+                  
+                        migrantRange["A" + (4 + cont).ToString()].Text = "Requerimiento Neto";
+                   
+                        migrantRange["A" + (5 + cont).ToString()].Text = "Liberación Orden";
+                    
+                        migrantRange["A" + (6 + cont).ToString()].Text = "Inventario Final";
+                    
+                    //Recorrido de columnas desde la columna 2 en adelante 
+                    for (int column = 2; column < int.Parse(CantSemanas) + 2; column++)
+                        {
+                         
+                            migrantRange.ResetRowColumn(cont+1, column);
+                            migrantRange.Text = "Día" + (column - 1).ToString()+"  "+ DateSelected.Date.AddDays(column-2).ToString().Remove(10); ;
+                            migrantRange.ResetRowColumn(cont+2, column);
+                            migrantRange.Text = productoA.Semanas[column - 2].ReqBruto;
+                            migrantRange.ResetRowColumn(cont+3, column);
+                            migrantRange.Text = productoA.Semanas[column - 2].InventarioInicial;
+                            migrantRange.ResetRowColumn(cont+4, column);
+                            migrantRange.Text = productoA.Semanas[column - 2].ReqNeto;
+                            migrantRange.ResetRowColumn(cont+5, column);
+                            migrantRange.Text = productoA.Semanas[column - 2].LibOrden;
+                            migrantRange.ResetRowColumn(cont+6, column);
+                            migrantRange.Text = productoA.Semanas[column - 2].InvFinal;
+                        }
+                    cont = cont + 7;
+                    worksheet.UsedRange.AutofitColumns();
+                }
+                //Save the workbook to stream in xlsx format. 
+                    MemoryStream stream = new MemoryStream();
+                    workbook.SaveAs(stream);
+
+                    workbook.Close();
+                
+                //Save the stream as a file in the device and invoke it for viewing
+              await DependencyService.Get<ISave>().SaveAndView(ProductoPrincipal.Nombre+".xlsx", "application/msexcel", stream);
+
+                await dialogService.ShowMessage("Mensaje", "El Producto se guardó como hoja de Excel en la carpeta MRP");
+            }
+        }
         private void ReCalcular()
         {
            
